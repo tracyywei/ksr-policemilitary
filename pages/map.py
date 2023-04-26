c97cdcb21c49db9c029799abd75d32b00510aff8
Tracy Wei <tracywei27@gmail.com>
2023-04-26 15:55:09 -0700

    leas clickable

--- pages/map.py
+++ pages/map.py
@@ -7,11 +7,7 @@ from urllib.request import urlopen
 import json
 from copy import deepcopy
 from plotly.subplots import make_subplots
-import dash
-from dash.dependencies import Input, Output, State
-import dash_core_components as dcc
-import dash_html_components as html
-import threading
+from streamlit_plotly_events import plotly_events
 
 import os
 # checking that the current working directory is correct to ensure that the file paths are working
@@ -37,18 +33,9 @@ leas = pd.read_csv("data/LEA-20230424.csv")
 # Geographic Map
 st.title("Map: Military Equipment Inventory in Law Enforcement Agencies")
 
-# Define a callback function to handle clicks on markers
-def on_selected(trace, points, selector):
-    selected_name = trace.hovertext[points.point_inds[0]]
-    print(f"Selected name: {selected_name}")
-
-# Initialize the state variable
-state = {"selected_name": None}
-
 fig = go.Figure()
 
-fig.add_trace(
-    go.Choroplethmapbox(
+choro_trace = go.Choroplethmapbox(
         geojson=geo,
         locations=df.name,
         featureidkey="properties.COUNTY_NAME",
@@ -59,10 +46,8 @@ fig.add_trace(
         hovertemplate="<b>%{location} County</b><br>Population: %{z:,}",
         customdata=df.name
     )
-)
 
-fig.add_trace(
-    go.Scattermapbox(
+scatter_trace = go.Scattermapbox(
         lat = leas['Latitude'],
         lon = leas['Longitude'],
         mode="markers",
@@ -78,26 +63,26 @@ fig.add_trace(
             }
         }
     )
-)
-
-# Set the on_click event handler for the scattermapbox trace
-fig.data[1].on_selection(on_selected)
 
 fig.update_layout(
     mapbox_style="carto-positron",
     mapbox_zoom=5,
     mapbox_center={"lat": 37, "lon": -120},
     width=800,
-    height=600,
+    height=800,
     margin={"r": 0, "t": 0, "l": 0, "b": 0},
     clickmode='event+select'
 )
 
-st.plotly_chart(fig)
+fig.add_trace(choro_trace)
+fig.add_trace(scatter_trace)
 
-# Access the selected name from the state variable
-selected_name = state["selected_name"]
-print(f"Selected name: {selected_name}")
+selected_points = plotly_events(fig)
+selected_lea = ""
+for point in selected_points :
+    index = point['pointIndex']
+    leaID = leas.iloc[index]['Ref']
+    selected_lea = leaID
 
 
 # Define the data table
@@ -107,20 +92,12 @@ def load_lea_data():
     return df
 
 lea_df = load_lea_data()
-filtered_data = lea_df 
 
+def filter_table():
+    filtered_entries = lea_df[lea_df['Ref'] == selected_lea]
+    return filtered_entries
 
-# # Add an event handler for the plotly_click event
-# def on_click(trace, points, state):
-#     global filtered_data
-#     console.log("HIIII")
-#     if points.marker.color[0]:
-#         clicked_county = points.customdata[0]  # get the clicked county name
-#         filtered_data = lea_df[lea_df['County'] == clicked_county]
-#         table = st.table(filtered_data)
-
-
-table = st.table(filtered_data)
-
-# # Bind the event handler to the click event
-# fig.data[0].on_click(on_click)
+filtered_df = filter_table()
+if(not filtered_df.empty) :
+    st.header(filtered_df['LEA_Name'])
+    table = st.table(filtered_df)
